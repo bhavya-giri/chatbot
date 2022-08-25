2226e5a8e2b7dcb8d44f26c1cc95bf1e60057753
Bhavya Giri <bhavya.giri44@gmail.com>
2022-08-25 17:33:46 +0530

    app 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from "react";
 import Chatbot from "react-chatbot-kit";
+import './App.css'
+
+import ActionProvider from './ActionProvider'
+import MessageParser from './MessagerParser'
+import config from './config'
 
 function App() {
   return (
     <div className="App">
      <header className="App-header">
-      <Chatbot/>
+      <Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser}/>
      </header>
     </div>
   );
